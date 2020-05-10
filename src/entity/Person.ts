5e74cd80ffe0d88623bc777db57a1d00f3c89a47
Kamil Kowalewski <szamil128@onet.pl>
2020-05-10 17:16:12 +0200

    Add database config

---
+++ src/entity/Person.ts
@@ -0,0 +1,14 @@
+import PrimaryEntity from "./base/PrimaryEntity";
+import {Entity} from "typeorm";
+
+@Entity()
+export class Person extends PrimaryEntity {
+
+    /*------------------------ FIELDS REGION ------------------------*/
+
+    /*------------------------ METHODS REGION ------------------------*/
+
+}
+
+export default Person;
+    
