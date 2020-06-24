c44d56f5052e8307487bfdc333d33da849b57d81
Kamil Kowalewski <szamil128@onet.pl>
2020-06-24 17:54:50 +0200

    Add partially done service layer

--- src/loader/DataLoader.ts
+++ src/loader/DataLoader.ts
@@ -1,42 +1,48 @@
-import {Connection, Repository} from "typeorm";
+import {Connection} from "typeorm";
 import {Developer} from "../entity/person/Developer";
 import {Technology} from "../entity/technology/Technology";
 import {Client} from "../entity/person/Client";
-import {TechnologyType} from "../entity/technology/TechnologyType";
+import {ClientServiceImpl} from "../service/client/ClientServiceImpl";
+import {DeveloperServiceImpl} from "../service/developer/DeveloperServiceImpl";
+import {TechnologyServiceImpl} from "../service/technology/TechnologyServiceImpl";
 
 export class DataLoader {
 
     /*------------------------ FIELDS REGION ------------------------*/
     private readonly _connection: Connection;
-    private readonly _developerRepository: Repository<Developer>;
-    private readonly _technologyRepository: Repository<Technology>;
-    private readonly _clientRepository: Repository<Client>;
+    private readonly _clientService: ClientServiceImpl;
+    private readonly _developerService: DeveloperServiceImpl;
+    private readonly _technologyService: TechnologyServiceImpl;
 
     /*------------------------ METHODS REGION ------------------------*/
-    constructor(connection: Connection) {
+    public constructor(connection: Connection) {
         this._connection = connection;
-        //todo add service layer and change here
-        this._developerRepository = this._connection.getRepository(Developer);
-        this._technologyRepository = this._connection.getRepository(Technology);
-        this._clientRepository = this._connection.getRepository(Client);
+
+        this._clientService = new ClientServiceImpl(
+                this._connection.getRepository(Client));
+
+        this._developerService = new DeveloperServiceImpl(
+                this._connection.getRepository(Developer));
+
+        this._technologyService = new TechnologyServiceImpl(
+                this._connection.getRepository(Technology));
     }
 
-    async loadData() {
-        const clientOne: Client = new Client("Artur", "Kowalewski");
-        const clientTwo: Client = new Client("Krzysztof", "Kowalewski");
-        const technology: Technology = new Technology("NodeJs", TechnologyType.BACK_END);
-        const developer: Developer = new Developer("Kamil", "Kowalewski");
-
-        technology.developer = developer;
-        developer.clients = [clientOne, clientTwo];
-
-        await this._technologyRepository.save(technology);
-        console.log(await this._developerRepository.findOne(8));
+    public async loadData() {
+        // const clientOne: Client = new Client("Artur", "Kowalewski");
+        // const clientTwo: Client = new Client("Krzysztof", "Kowalewski");
+        // const technology: Technology = new Technology("NodeJs", TechnologyType.BACK_END);
+        // const developer: Developer = new Developer("Kamil", "Kowalewski");
+        //
+        // technology.developer = developer;
+        // developer.clients = [clientOne, clientTwo];
+        //
+        // await this._technologyService.save(technology);
     }
 
-    async removeData() {
-        // await this._clientRepository.clear();
-        await this._developerRepository.clear();
-        await this._technologyRepository.clear();
+    public async removeData() {
+        // await this._clientService.deleteAll();
+        // await this._developerService.deleteAll();
+        // await this._technologyService.deleteAll();
     }
 }
--- src/service/BaseService.ts
+++ src/service/BaseService.ts
@@ -1,4 +1,19 @@
 export interface BaseService<T> {
 
-    // findById(id: number): T;
+    // TODO
+    // findById(id: number): Promise<T>;
+    //
+    // findByObject(object: T): Promise<T>;
+    //
+    // findAll(): Promise<T[]>;
+    //
+    // update(object: T): Promise<T>;
+    //
+    // save(object: T): Promise<T>;
+    //
+    // deleteById(id: number): Promise<T>;
+    //
+    // delete(object: T): Promise<T>;
+    //
+    // deleteAll(): Promise<void>;
 }
