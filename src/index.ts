d6e41467a4bb29bd7c18cea2966248fe8c070375
Kamil Kowalewski <szamil24@gmail.com>
2020-08-07 13:58:46 +0200

    Add displaying api paths

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,21 @@ import "reflect-metadata";
 import {connection} from "./connection/Connection";
 import {DataLoader} from "./loader/DataLoader";
 import {Application} from "./Application";
-import {PORT} from "./constant/Constants";
+import {
+    PATH_API_DOCS, PATH_API_PREFIX, PATH_CLIENT, PATH_DEVELOPER, PATH_TECHNOLOGY, PORT
+} from "./constant/Constants";
+
+const printApiPaths = (): void => {
+    console.log();
+    console.log("PATH_API_DOCS: " + PATH_API_DOCS);
+    console.log("PATH_API_PREFIX: " + PATH_API_PREFIX);
+    console.log("PATH_CLIENT: " + PATH_CLIENT);
+    console.log("PATH_DEVELOPER: " + PATH_DEVELOPER);
+    console.log("PATH_TECHNOLOGY: " + PATH_TECHNOLOGY);
+    console.log();
+};
+
+printApiPaths();
 
 export const application: Application = new Application();
 
